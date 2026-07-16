7410195c29773f44b39bbf45ccc48cb2b19b511b
Rudraksh0076 <rudrakshjames@gmail.com>
2026-07-16 18:05:32 +0530

    Create hollow-diamond.cpp
    add hollow-diamond.cpp

---
+++ hollow-diamond.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the number of lines:";
+    cin >> n;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+        cout << "*";
+        if(i!=0){
+            for(int j=0; j<2*i-1; j++){
+                cout <<" ";
+            }
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<i+1; j++){
+            cout<<" ";
+        }
+        cout << "*";
+        if(i<n-2){
+            for(int j=0; j<2*(n-i-2)-1; j++){
+                cout << " ";
+            }
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
